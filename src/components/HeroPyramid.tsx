26f280693699cac2ce55cf1e8e886c8b367cc497
Sarim Rayyan <sarimryan@gmail.com>
2026-09-02 18:04:32 +0500

    Exact UI alignment to Google Drive video: background grid spacing, tetrahedron 3D geometry, and theme variables

--- src/components/HeroPyramid.tsx
+++ src/components/HeroPyramid.tsx
@@ -5,7 +5,7 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { Float } from '@react-three/drei';
 import * as THREE from 'three';
 
-function TriforcePyramidGlass({ isDark }: { isDark: boolean }) {
+function TriforceExactMesh({ isDark }: { isDark: boolean }) {
   const groupRef = useRef<THREE.Group>(null!);
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
 
@@ -21,73 +21,69 @@ function TriforcePyramidGlass({ isDark }: { isDark: boolean }) {
 
   useFrame((_, delta) => {
     if (groupRef.current) {
-      // Continuous floating rotation matching video_frame_1 & video_frame_6
-      groupRef.current.rotation.y += delta * 0.25;
-
-      // Mouse tracking lerp
-      groupRef.current.rotation.x = THREE.MathUtils.lerp(groupRef.current.rotation.x, mouse.y * 0.35, 0.05);
+      groupRef.current.rotation.y += delta * 0.2;
+      groupRef.current.rotation.x = THREE.MathUtils.lerp(groupRef.current.rotation.x, mouse.y * 0.3, 0.05);
       groupRef.current.rotation.y = THREE.MathUtils.lerp(groupRef.current.rotation.y, groupRef.current.rotation.y + mouse.x * 0.02, 0.05);
     }
   });
 
   const greenAccent = "#3DDC84";
-  const faceColor = isDark ? "#0A1F13" : "#D1FADB";
+  const bodyColor = isDark ? "#0E2417" : "#DCFCE7";
 
   return (
-    <Float speed={2} rotationIntensity={0.4} floatIntensity={0.8}>
-      <group ref={groupRef} scale={1.75} position={[0, 0, 0]}>
-        {/* Top Triangular Prism */}
-        <group position={[0, 0.85, 0]}>
+    <Float speed={1.5} rotationIntensity={0.3} floatIntensity={0.5}>
+      <group ref={groupRef} scale={1.8}>
+        {/* Top Tetrahedron */}
+        <group position={[0, 0.86, 0]}>
           <mesh>
-            <coneGeometry args={[0.9, 1.2, 3]} />
+            <tetrahedronGeometry args={[0.9, 0]} />
             <meshStandardMaterial
-              color={faceColor}
-              transparent={true}
-              opacity={isDark ? 0.75 : 0.8}
-              roughness={0.2}
-              metalness={0.5}
+              color={bodyColor}
+              roughness={0.1}
+              metalness={0.8}
+              transparent
+              opacity={0.85}
             />
           </mesh>
-          {/* Green Border Outline */}
           <mesh>
-            <coneGeometry args={[0.905, 1.205, 3]} />
-            <meshBasicMaterial color={greenAccent} wireframe={true} wireframeLinewidth={2} />
+            <tetrahedronGeometry args={[0.905, 0]} />
+            <meshBasicMaterial color={greenAccent} wireframe wireframeLinewidth={2} />
           </mesh>
         </group>
 
-        {/* Bottom Left Triangular Prism */}
-        <group position={[-0.85, -0.6, 0]}>
+        {/* Bottom Left Tetrahedron */}
+        <group position={[-0.86, -0.6, 0]}>
           <mesh>
-            <coneGeometry args={[0.9, 1.2, 3]} />
+            <tetrahedronGeometry args={[0.9, 0]} />
             <meshStandardMaterial
-              color={faceColor}
-              transparent={true}
-              opacity={isDark ? 0.75 : 0.8}
-              roughness={0.2}
-              metalness={0.5}
+              color={bodyColor}
+              roughness={0.1}
+              metalness={0.8}
+              transparent
+              opacity={0.85}
             />
           </mesh>
           <mesh>
-            <coneGeometry args={[0.905, 1.205, 3]} />
-            <meshBasicMaterial color={greenAccent} wireframe={true} wireframeLinewidth={2} />
+            <tetrahedronGeometry args={[0.905, 0]} />
+            <meshBasicMaterial color={greenAccent} wireframe wireframeLinewidth={2} />
           </mesh>
         </group>
 
-        {/* Bottom Right Triangular Prism */}
-        <group position={[0.85, -0.6, 0]}>
+        {/* Bottom Right Tetrahedron */}
+        <group position={[0.86, -0.6, 0]}>
           <mesh>
-            <coneGeometry args={[0.9, 1.2, 3]} />
+            <tetrahedronGeometry args={[0.9, 0]} />
             <meshStandardMaterial
-              color={faceColor}
-              transparent={true}
-              opacity={isDark ? 0.75 : 0.8}
-              roughness={0.2}
-              metalness={0.5}
+              color={bodyColor}
+              roughness={0.1}
+              metalness={0.8}
+              transparent
+              opacity={0.85}
             />
           </mesh>
           <mesh>
-            <coneGeometry args={[0.905, 1.205, 3]} />
-            <meshBasicMaterial color={greenAccent} wireframe={true} wireframeLinewidth={2} />
+            <tetrahedronGeometry args={[0.905, 0]} />
+            <meshBasicMaterial color={greenAccent} wireframe wireframeLinewidth={2} />
           </mesh>
         </group>
       </group>
@@ -102,15 +98,15 @@ export default function HeroPyramid({ isDark = true }: { isDark?: boolean }) {
     setMounted(true);
   }, []);
 
-  if (!mounted) return <div className="w-full h-full min-h-[400px]" />;
+  if (!mounted) return <div className="w-full h-full min-h-[420px]" />;
 
   return (
-    <div className="w-full h-[400px] md:h-[520px] relative">
-      <Canvas camera={{ position: [0, 0, 5.5], fov: 45 }}>
-        <ambientLight intensity={1.2} />
-        <directionalLight position={[10, 15, 10]} intensity={2.0} color="#3DDC84" />
-        <directionalLight position={[-10, -10, -5]} intensity={1.0} color="#FFFFFF" />
-        <TriforcePyramidGlass isDark={isDark} />
+    <div className="w-full h-[420px] md:h-[560px] relative">
+      <Canvas camera={{ position: [0, 0, 5.2], fov: 45 }}>
+        <ambientLight intensity={1.5} />
+        <directionalLight position={[10, 15, 10]} intensity={2.5} color="#3DDC84" />
+        <directionalLight position={[-10, -10, -5]} intensity={1.2} color="#FFFFFF" />
+        <TriforceExactMesh isDark={isDark} />
       </Canvas>
     </div>
   );
